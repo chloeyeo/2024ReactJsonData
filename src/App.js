03e47cb9bbd3b7091bbf4244524118c754f4a7b8
Chloe Yeo <yeochloe01@gmail.com>
2024-04-09 10:58:04 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, NavLink } from "react-router-dom";
+import React from "react";
 
 function App() {
+  const mainMenu = [
+    { name: "회사소개", url: "#" },
+    { name: "제품소개", url: "#" },
+    { name: "회사소개", url: "#" },
+    { name: "온라인문의", url: "#" },
+    { name: "커뮤니티", url: "#" },
+  ]; // static thus don't need useState
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="header">
+        <div className="container">
+          <h1>logo</h1>
+          <nav>
+            <ul>
+              {mainMenu.map((item, i) => {
+                return (
+                  // key must be unique
+                  <li key={i}>
+                    <a href={item.url}>{item.name}</a>
+                  </li>
+                );
+              })}
+            </ul>
+          </nav>
+        </div>
+      </div>
+      <Routes>
+        <Route path="/" />
+      </Routes>
+    </>
   );
 }
 
